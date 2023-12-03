f910c8144211308db906af4e893f24a45c6a5cf6
Henrique GC <henriq4NET@gmail.com>
2023-12-03 01:59:07 -0300

    feat: styled date picker

--- src/components/Form/DatePicker/component.tsx
+++ src/components/Form/DatePicker/component.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { TouchableOpacity } from "react-native";
-import { Text } from "native-base";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View } from "native-base";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { useDatePicker } from "./hook";
+import { CalendarIcon } from "../../Icons/CalendarIcon";
 
 export function DatePicker() {
   const { date, show, onDataChange, setShow } = useDatePicker();
@@ -19,12 +20,30 @@ export function DatePicker() {
       )}
 
       <TouchableOpacity
+        style={styles.container}
         onPress={() => {
           setShow(true);
         }}
       >
-        <Text>Ola</Text>
+        <Text color="#8C8C8C">Ola</Text>
+        <CalendarIcon />
       </TouchableOpacity>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: 48,
+    width: "100%",
+    borderColor: "#8C8C8C",
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
